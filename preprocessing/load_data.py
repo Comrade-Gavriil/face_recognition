0476f6f42785a072512573a1b52dac0b88cdc9a2
Graham Dobbie <comradegavriil@gmail.com>
2020-01-18 01:03:23 -0700

    improved download

--- preprocessing/load_data.py
+++ preprocessing/load_data.py
@@ -1,10 +1,10 @@
 from google_images_download import google_images_download
 
 
-def download_images (args)
+def download_images (args):
     response = google_images_download.googleimagesdownload()
     absolute_image_paths = response.download(args)
 
-args = {'keywords':'faces', 'limit':'1000', 'output_directory': '../images'}
+args = {'keywords':'faces', 'limit':'10', 'output_directory': '../images'}
 
 download_images(args)
