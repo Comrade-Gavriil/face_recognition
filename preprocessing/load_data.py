361d9f378a4c5153224f838331722bcf2792d77d
Graham Dobbie <comradegavriil@gmail.com>
2020-01-18 01:05:58 -0700

    more images

--- preprocessing/load_data.py
+++ preprocessing/load_data.py
@@ -5,6 +5,6 @@ def download_images (args):
     response = google_images_download.googleimagesdownload()
     absolute_image_paths = response.download(args)
 
-args = {'keywords':'faces', 'limit':'10', 'output_directory': '../images'}
+args = {'keywords':'faces', 'limit':'1000', 'output_directory': '../images'}
 
 download_images(args)
